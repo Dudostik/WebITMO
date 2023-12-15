3ace70ee60bbbb89168f9d3010f1e02e13ef383e
Dudostik <sergeev_rostislav@mail.ru>
2023-12-15 14:46:52 +0300

    lab4

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
 //==============
 
-//Menu
-let iconMenu = document.querySelector(".icon-menu");
-if (iconMenu != null) {
-	let delay = 500;
-	let body = document.querySelector("body");
-	let menuBody = document.querySelector(".menu__body");
-	iconMenu.addEventListener("click", (e) => {
-		if (!body.classList.contains('_wait')) {
-			body_lock(delay);
-			iconMenu.classList.toggle("_active");
-			menuBody.classList.toggle("_active");
-		}
-	});
-};
-
-function menu_close() {
-	let iconMenu = document.querySelector(".icon-menu");
-	let menuBody = document.querySelector(".menu__body");
-	iconMenu.classList.remove("_active");
-	menuBody.classList.remove("_active");
-}
+(function() {
+  window.addEventListener('load', function() {
+    setTimeout(function() {
+      var perfEntries = performance.getEntriesByType('navigation');
+      if (perfEntries.length > 0) {
+        var p = perfEntries[0];
+        var loadTime = p.loadEventEnd - p.startTime;
+        var loadTimeElement = document.getElementById('loadTime');
+        loadTimeElement.innerHTML = 'Page load time: ' + loadTime + 'ms';
+      }
+    });
+  });
+})();
 
 //==============
